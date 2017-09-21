b05197443fb53ed6dfa299d0b13f4cb4ae6e3a11
Brandon Kudo and Rakhee Gandhi <rakhee@>
2017-09-21 11:37:58 -0700

    Add functionality to replace inputted numbers with tens and hundredths places.

--- js/scripts.js
+++ js/scripts.js
@@ -22,16 +22,38 @@
 $(document).ready(function() {
   $("#romanForm").submit(function(event){
     //var numbers=["0, 1, 2, 3, 4, 5, 6, 7, 8, 9"]
-
-    //var numberOne= "1"
-
     var input= $("input#numberInput").val();
+    //var numberOne= "1"
+    if (input.length === 1) {
+      var oneReplace = input.replace(/1/g, "I").replace(/2/g, "II").replace(/3/g,"III").replace(/4/g, "IV").replace(/5/g, "V").replace(/6/g, "VI").replace(/7/g, "VII").replace(/8/g, "VIII").replace(/9/g, "IX");
 
-    var inputReplace = input.replace(/1/g, " I");
+      $("#output").show();
+      $("#output1").text(oneReplace);
+
+    }
+    if (input.length === 2) {
+      var tenReplace = input.charAt(0).replace("1", "X").replace("2", "XX").replace("3", "XXX").replace("4", "XL").replace("5", "L").replace("6", "LX").replace("7", "LXX").replace("8", "LXXX").replace("9", "XC");
+      var ten2Replace = input.charAt(1).replace(/1/g, "I").replace(/2/g, "II").replace(/3/g,"III").replace(/4/g, "IV").replace(/5/g, "V").replace(/6/g, "VI").replace(/7/g, "VII").replace(/8/g, "VIII").replace(/9/g, "IX");
+
+      $("#output").show();
+      $("#output1").text(tenReplace + ten2Replace);
+    }
+    if (input.length === 3) {
+      var hundredReplace = input.chartAt(0).replace("1", "C").replace("2", "CC").replace("3", "CCC").replace("4", "CD").replace("5","D").replace("6", "DC").replace("7", "DCC").replace("8", "DCCC").replace("9" , "CM");
+      var hundred2Replace = input.charAt(1).replace("1", "X").replace("2", "XX").replace("3", "XXX").replace("4", "XL").replace("5", "L").replace("6", "LX").replace("7", "LXX").replace("8", "LXXX").replace("9", "XC");
+      var hundred3Replace = input.charAt(2).replace(/1/g, "I").replace(/2/g, "II").replace(/3/g,"III").replace(/4/g, "IV").replace(/5/g, "V").replace(/6/g, "VI").replace(/7/g, "VII").replace(/8/g, "VIII").replace(/9/g, "IX");
+
+      $("#output").show();
+      $("#output1").text(hundredReplace + hundred2Replace + hundred3Replace);
+    }
 
 
-    $("#output").show();
-    $("#output1").text(inputReplace);
+    //var oneReplace = input.replace(/1/g, "I").replace(/5/g, "V");
+    //var fiveReplace = input.replace(/5/g, "V");
+
+    //
+    // $("#output").show();
+    // $("#output1").text(oneReplace);
 
     event.preventDefault();
   });
